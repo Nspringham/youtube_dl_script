5e6c210735d2ad75abd112ee59794e38c16c0906
Nick Springham <Nspringham@aol.co.uk>
2018-12-08 19:05:16 +0000

    altered main and added to readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # youtube_dl_script
-cmd line script using hte youtube-dl module
+
+cmd line python script using the youtube-dl module
+
+make sure you have youtube-dl
+ffmpeg
+and python3
+
--- youtube-dl.py
+++ youtube-dl.py
@@ -1,28 +1,28 @@
 ## Small command line program to save time when using the Youtube-dl module
 ## Nick Springham
 
-# update youtube-dl
-
 import os
 import time
 import subprocess
 import pyperclip
 
 
-
 command = r"youtube-dl --extract-audio --audio-format mp3 -o %(title)s.%(ext)s "
+
 user_input = input("\nEnter full Youtube URL: ")
 
 cmd_call = command + user_input
 
-# change directory
+# change working directory to desired destination directory
 directory = r"C:\enter\destination\directory"
 os.chdir(directory)
 
 time.sleep(1)
+
 print("\n" + "Running: " + cmd_call + "\n")
 print("This file will be found in: " + os.getcwd() + "\n")
-# run the youtube dl
 
-#os.system(cmd_call)
+# run the youtube-dl
+os.system(cmd_call)
+# opens the desination folder when download is finished
 subprocess.Popen('explorer '+ directory)
